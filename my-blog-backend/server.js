0dfabf2f4e703cd0df20d03a832567e71f5ac557
sehoonk2004 <107429645+sehoonk2004@users.noreply.github.com>
2024-06-11 17:20:45 -0400

    Serve production build v2

--- my-blog-backend/server.js
+++ my-blog-backend/server.js
@@ -35,10 +35,10 @@ app.get('/api/posts/:id', async (req, res) => {
 });
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../my-blog/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../my-blog/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
